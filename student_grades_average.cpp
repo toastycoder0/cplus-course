f32595eb2c6c3a34a0d8717c6361fff55f508e21
toastycoder0 <re-jos2002@proton.me>
2025-07-29 21:19:33 -0600

    feat: added grades average script

---
+++ student_grades_average.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  const int GRADES_TO_READ = 7;
+  double studentGradesTotal = 0;
+  string studentName;
+
+  cout << "Ingresa el nombre del alumno: ";
+  getline(cin, studentName);
+
+  for (int i = 0; i < GRADES_TO_READ; i++) {
+    double inputGrade;
+
+    while (true) {
+      cout << "Ingresa el valor de la calificación " << i + 1 << ": ";
+      cin >> inputGrade;
+
+      if (cin.fail()) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Entrada no válida. Por favor ingresa un número valido.\n";
+        continue;
+      }
+
+      studentGradesTotal += inputGrade;
+      break;
+    }
+  }
+
+  double gradeAverage = studentGradesTotal / GRADES_TO_READ;
+
+  cout << "El promedio del estudiante " << studentName << " es de "
+       << gradeAverage << "\n";
+
+  return 0;
+}
